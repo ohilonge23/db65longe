9aa95a296fc602f2d35c7d8c4335ff4fd1889ee5
ohilonge <54418880+ohilonge23@users.noreply.github.com>
2021-11-04 18:51:45 -0500

    Part 7 completed
    Created selector.pug and js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/selector');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jobsRouter = require('./routes/jobs');
 var addmodsRouter = require('./routes/addmods');
