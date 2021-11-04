72434a91e0b698f13dfc7b08a5a360b50fb934b7
ohilonge <54418880+ohilonge23@users.noreply.github.com>
2021-11-04 18:05:31 -0500

    Part 8 : Pulling in images
    selector.js and pug files added

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/selector');
 var usersRouter = require('./routes/users');
 var jobsRouter = require('./routes/jobs');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jobs', jobsRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
