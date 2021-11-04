f9dfee99f721a3ac16ea643ae7718b69ec98215e
ohilonge <54418880+ohilonge23@users.noreply.github.com>
2021-11-04 17:44:31 -0500

    Part 6 file added

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('movies', { title: 'Search Results' });
+  res.render('jobs', { title: 'Search Results' });
 });
 
 module.exports = router;
